442ee26e218d9946a196a503f47f80f6996246cc
Abu Osama Khan <khankhanosama19@gmail.com>
2023-05-12 03:28:38 -0700

    first commit

---
+++ src/components/Videos.jsx
@@ -0,0 +1,27 @@
+import { Stack, Box } from "@mui/material";
+import React from "react";
+
+import { ChannelCard, VideoCard } from "./";
+
+
+const Videos = ({videos, direction}) => {
+  //console.log("videos.....", videos);
+   
+  return (
+    <Stack direction= {direction || "row"} flexWrap="wrap" justifyContent="start" gap={2}>
+
+    {videos && videos.map((item, idx) => (
+      <Box key={idx}>
+      {item.id.videoId && <VideoCard video={item} /> }
+      {item.id.channelId && <ChannelCard channelDetail={item} />}
+    </Box>
+      ))
+    };
+
+    </Stack>
+  );
+};
+
+
+
+export default Videos;
